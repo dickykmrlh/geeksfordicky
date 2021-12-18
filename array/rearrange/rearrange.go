ef315755434d182d7b9db088f6c6c52715d15675
Dicky Kamarulah <dicky@munalively.com>
2021-12-18 11:36:38 +0700

    another improvement with map, with this needed extra space, but the time complexity became n + n or 2n or just N

--- array/rearrange/rearrange.go
+++ array/rearrange/rearrange.go
@@ -1,30 +1,25 @@
 package rearrange
 
-import "sort"
-
 /*
 SortArrayValueToIndexPosition rearrange array such that the value
 match the index position of the array, if the value can't be found for
 that particular index, replace it with -1
  */
 func SortArrayValueToIndexPosition(arr []int) []int{
-	var results []int
-	sort.Ints(arr)
-	i, j := 0, 0
+	m := make(map[int]int)
 
-	for ; i < len(arr); i++ {
-		for j < len(arr) {
-			if i == arr[j] {
-				results = append(results, arr[j])
-				j++
-				break
-			} else if i < arr[j] {
-				results = append(results, -1)
-				break
-			}
-			j++
+	for _, v := range arr {
+		m[v] = v
+	}
+
+	for i, _ := range arr {
+		val, ok := m[i]
+		if ok {
+			arr[i] = val
+		} else {
+			arr[i] = -1
 		}
 	}
 
-	return results
+	return arr
 }
